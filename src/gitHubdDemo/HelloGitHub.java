3a3558f14274de4a60f3b60ad16c6b8116ec8130
shahab6671 <sbuddin6671@gmail.com>
2023-06-03 00:55:25 -0400

    githubdemo

---
+++ src/gitHubdDemo/HelloGitHub.java
@@ -0,0 +1,9 @@
+package gitHubdDemo;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("I know how to create a separate remote repository in Github and can push my project GithubDemo, without the help of anyone");
+	}
+
+}
